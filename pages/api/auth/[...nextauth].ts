4fdf8e4e41c1312ba0f040fdc1cf4b3f73b87d37
Giao Vu Dinh <tvudinh20@gmail.com>
2025-03-08 13:55:55 -0500

    fix typing issues

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,12 +8,12 @@ import prisma from "../../../lib/prisma";
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
     Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      clientId: process.env.AUTH0_CLIENT_ID ?? "",
+      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? "",
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
