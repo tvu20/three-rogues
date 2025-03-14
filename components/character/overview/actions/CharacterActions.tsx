f9da4bc341ea09c843dbdebed2061e9200bb56c3
Giao Vu Dinh <tvudinh20@gmail.com>
2025-03-14 01:57:20 -0400

    create display feature block

--- components/character/overview/actions/CharacterActions.tsx
+++ components/character/overview/actions/CharacterActions.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "next/navigation";
 import { useGetCharacterQuery } from "../../../../app/api/apiSlice";
+import { LINKED_ABILITY } from "../../../../app/character/characterDefs";
 import Loader from "../../../shared/Loader";
+import FeatureBlock from "../../shared/FeatureBlock";
 import styles from "./CharacterActions.module.css";
+
 const CharacterActions = () => {
   const params = useParams<{ id: string }>();
   const id = params?.id || "";
@@ -14,6 +17,22 @@ const CharacterActions = () => {
     return <Loader />;
   }
 
+  const createFeatureBlocks = (linkedAbility: LINKED_ABILITY) => {
+    const features = character?.features.filter(
+      (feature) => feature.linkedAbility === linkedAbility
+    );
+
+    return features?.map((feature) => (
+      <FeatureBlock
+        key={feature.id}
+        title={feature.name}
+        description={feature.description}
+        className={styles.featureBlock}
+        options={feature.options}
+      />
+    ));
+  };
+
   return (
     <div className={`content-box ${styles.container}`}>
       <div className={styles.section}>
@@ -21,19 +40,25 @@ const CharacterActions = () => {
           Actions
           <span>Attacks per action: {character?.attacksPerAction || "1"}</span>
         </h2>
-        <p>
+        <p className={styles.subtitle}>
           Attack, Dash, Disengage, Dodge, Grapple, Help, Hide, Improvise,
           Influence, Magic, Ready, Search, Shove, Study, Utilize
         </p>
+        {createFeatureBlocks("action")}
       </div>
       <div className={styles.section}>
         <h2 className={styles.title}>Bonus Actions</h2>
+        <p className={styles.subtitle}>Two-Weapon Fighting</p>
+        {createFeatureBlocks("bonusaction")}
       </div>
       <div className={styles.section}>
         <h2 className={styles.title}>Reactions</h2>
+        <p className={styles.subtitle}>Opportunity Attack</p>
+        {createFeatureBlocks("reaction")}
       </div>
-      <div className={styles.section}>
+      <div className={`${styles.section} ${styles.otherSection}`}>
         <h2 className={styles.title}>Other</h2>
+        {createFeatureBlocks("other")}
       </div>
     </div>
   );
---
+++ components/character/shared/FeatureBlock.tsx
@@ -0,0 +1,57 @@
+import { LINKED_ABILITY } from "../../../app/character/characterDefs";
+import styles from "./FeatureBlock.module.css";
+
+const LINKED_ABILITY_LABELS = {
+  action: "Action",
+  bonusaction: "Bonus Action",
+  reaction: "Reaction",
+  other: "Other",
+};
+
+type FeatureBlockProps = {
+  title: string;
+  description: string;
+  level?: number;
+  linkedAbility?: LINKED_ABILITY;
+  options?: {
+    [key: string]: string;
+  } | null;
+  className?: string;
+};
+
+const FeatureBlock = ({
+  title,
+  description,
+  level,
+  linkedAbility,
+  options,
+  className,
+}: FeatureBlockProps) => {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <div className={styles.header}>
+        <h4>{title}</h4>
+        {level && <div className={styles.level}>Lv. {level}</div>}
+      </div>
+
+      {linkedAbility && (
+        <h6 className={level ? styles.marginUp : ""}>
+          Affects: {LINKED_ABILITY_LABELS[linkedAbility]}
+        </h6>
+      )}
+      <p>{description}</p>
+      {options && (
+        <div className={styles.options}>
+          {Object.entries(options).map(([key, value]) => (
+            <p key={key}>
+              <i>{key}: </i>
+              {value}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FeatureBlock;
